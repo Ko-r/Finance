89ae5bb9a7120e90ec74509af90051f24995fc3d
Ko-r <61397216+Ko-r@users.noreply.github.com>
2022-12-20 16:29:08 -0600

    Add files via upload

---
+++ fin/finance.py
@@ -0,0 +1,37 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+import yfinance as yf
+from datetime import datetime 
+
+plt.style.use('seaborn')
+
+msft = yf.Ticker('msft')
+stockinfo = msft.info
+
+print(msft.info)
+print(msft.recommendations) 
+print(msft.splits)
+print(msft.institutional_holders)
+print(type(msft.dividends))
+
+df = msft.dividends
+
+print(df)
+
+data = df.resample('Y').sum()
+data = data.reset_index()
+data['Year'] = data['Date'].dt.year
+
+plt.figure()
+plt.bar(data['Year'], data ['Dividends'])
+plt.ylabel('Dividend Yield ($)')
+plt.xlabel('Year')
+plt.title('Microsoft Dividen Histroy')
+plt.xlim(2002,2020)
+plt.show()
+
+
+
+
+
+
